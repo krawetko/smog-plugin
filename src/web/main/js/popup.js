7a6710e8b03c8b6e25bae7edc254728a763461af
krawetko <krawetko@gmail.com>
2014-02-02 23:51:18 +0100

    Options removed. Station is selected directly in popup.

--- src/web/main/js/popup.js
+++ src/web/main/js/popup.js
@@ -1,30 +1,90 @@
 function refreshPage() {
 
     function createTableColumn(renderedValue) {
-        return "<td>" + renderedValue + "</td>";
+        return $("<td/>",
+            {
+                text: renderedValue
+            }
+        );
     }
 
     chrome.storage.local.get(storage.stationStatistics, function (data) {
         var stationStatistics = data[storage.stationStatistics],
             stationPollutions = stationStatistics[smogApi.props.stationStatistics.pollutions],
             lastRefreshDate = stationStatistics[smogApi.props.stationStatistics.lastRefresh];
-        var pollutionsTableRows = [];
+
+        var $pollutionsTableRows = $("<table/>");
 
         $.each(stationPollutions, function (index, pollution) {
-            pollutionsTableRows.push("<tr>" +
-                createTableColumn(pollution[smogApi.props.stationStatistics.pollution.substance]) +
-                createTableColumn(pollution[smogApi.props.stationStatistics.pollution.maxSafeValuePercent] + "%")
-                + "</tr>")
+            $pollutionsTableRows.append(
+                $('<tr/>')
+                    .append(createTableColumn(pollution[smogApi.props.stationStatistics.pollution.substance]))
+                    .append(createTableColumn(pollution[smogApi.props.stationStatistics.pollution.maxSafeValuePercent] + "%"))
+            );
         });
 
-        $("<table/>", {
-            "class": "pollutions-table",
-            html: pollutionsTableRows.join("")
-        }).appendTo("#content");
-        $('#toptitle').text(stationStatistics[smogApi.props.stationStatistics.address]);
-        $('h5').children('.lastRefresh').text(lastRefreshDate)
+        $("#statistics").html($pollutionsTableRows[0]);
+        $('footer').find('.lastRefresh').text(lastRefreshDate)
+        $("#content").animate({opacity: 1});
     });
 }
 
+function renderAvailableOptions() {
+    chrome.storage.local.get(storage.availableStations, function (data) {
+        var availableStations = data[storage.availableStations],
+            $availableStationsSelect = $('select');
+
+        $.each(availableStations, function (index, station) {
+            $availableStationsSelect.append(buildStationOption(station));
+        });
+        setSelectedStation($availableStationsSelect);
+    });
+
+    function buildStationOption(stationLocation) {
+        return $('<option/>', {
+                value: stationLocation[smogApi.props.stationLocation.id],
+                text: stationLocation[smogApi.props.stationLocation.address] + ", " + stationLocation[smogApi.props.stationLocation.cityArea]
+            }
+        )
+    }
+
+    function setSelectedStation($availableStationsSelect) {
+        chrome.storage.local.get(storage.selectedStation, function (data) {
+            var selectedStation = data[storage.selectedStation];
+            if (selectedStation != undefined) {
+                $availableStationsSelect.val(selectedStation);
+            }
+        });
+    }
+}
+
+function submitOptions(selectedStation) {
+    $("#content").animate({opacity: 0});
+    chrome.storage.local.set(new Obj([storage.selectedStation, selectedStation]));
+}
+
+
+$(function () {
+
+    $('select').on('change', function () {
+        submitOptions($(this).val());
+    });
+})
+
+chrome.storage.onChanged.addListener(stationStatisticsChangedListener);
+
+function stationStatisticsChangedListener(storageChanges) {
+
+    function hasStationStatisticsChanged() {
+        return storageChanges.hasOwnProperty(storage.stationStatistics);
+    }
+
+    if (hasStationStatisticsChanged()) {
+        refreshPage();
+    }
+
+}
+
+renderAvailableOptions();
 
 refreshPage();
